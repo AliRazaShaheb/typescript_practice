f52f1c8f813d76a429d4918fcd76ee5fc1c8a4eb
ALI RAZA SHAHEB <89347148+AliRazaShaheb@users.noreply.github.com>
2022-08-11 21:02:25 +0530

    final cssClasses function

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,28 @@
-const cssClasses = (str1: string, str2: string) => {
-  // const str2Obj = (str: string) => {
-  //   const strArr = str.split(" ");
-  //   const obj: any = {};
-  //   let val: string;
-  //   for (val of strArr) {
-  //     obj[val] = val;
-  //   }
-  //   return obj;
-  // };
-  // let obj1 = str2Obj(str1);
-  // let obj2 = str2Obj(str2);
-  // let obj1n2 = Object.keys({ ...obj1, ...obj2 }).map((each) => each.split("-"));
-  // console.log(obj1n2);
-  const wholeStrArr = str1.concat(" ", str2).split(" ");
-  const uniqArrValue = (arr: string[]) => {
-    const obj: any = {};
-    const obj1: any = {};
-    for (let val of wholeStrArr) {
-      let [cl, value] = val.split("-");
-      if (!value) {
-        obj1[value] = value;
-      } else {
-        obj1[cl] = value;
-      }
-      obj[val] = val;
+export const cssClasses = (str1: string, str2: string) => {
+  const obj: any = {};
+  const arr: string[] = [];
+  const wholeStrArr = str2.concat(" ", str1).split(" ");
+  for (let val of wholeStrArr) {
+    const [cl, clVal] = val.split("-");
+    if (!clVal) {
+      obj[cl] = cl;
+    } else {
+      obj[cl] = clVal;
     }
-    console.log(obj1);
-    return Object.keys(obj).map((each) => {
-      return each;
-    });
-  };
-
-  return uniqArrValue(wholeStrArr).join(" ");
+  }
+  for (let [key, value] of Object.entries(obj)) {
+    if (key === value) {
+      arr.push(key);
+    } else {
+      arr.push(`${key}-${value}`);
+    }
+  }
+  return arr.join(" ");
 };
 
-let cl = "fixed p-8 pt-4 text-white w-full z-50";
+let cl1 = "fixed p-11 pt-4";
 
-// console.log(cssClasses(cl, "s-8 p-8 y-9 p-9 heool safkas askfss y-9"));
+console.log(cssClasses(cl1, "p-9, pt-5 s-96 "));
 
 // TO DO
 // unique classname, upcoming classes should replace the old classes if same class exist. value should replace the old value
-
-const input = [
-  { index: 0, value: 3 },
-  { index: 0, value: 3 },
-  { index: 0, value: 3 },
-  { index: 1, value: 3 },
-  { index: 1, value: 3 },
-  { index: 2, value: 3 },
-  { index: 2, value: 3 },
-  { index: 0, value: 3 }
-];
-
-let obj = input.reduce((res, curr) => {
-  // console.log(res)
-  if (res[curr.index]) res[curr.index].push(curr);
-  else Object.assign(res, { [curr.index]: [curr] });
-
-  return res;
-}, {});
-
-// console.log(obj);
